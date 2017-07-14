dfa9a1b410af68bce01198d2aa66359d1f321bfe
greenwoodma <mark@dcs.shef.ac.uk>
2017-07-14 10:01:07 +0100

    starting to add support for showing steps in an invoked workflow

--- src/main/java/com/github/jmchilton/blend4j/galaxy/beans/WorkflowInvocation.java
+++ src/main/java/com/github/jmchilton/blend4j/galaxy/beans/WorkflowInvocation.java
@@ -1,5 +1,6 @@
 package com.github.jmchilton.blend4j.galaxy.beans;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
@@ -9,6 +10,8 @@ import org.codehaus.jackson.annotate.JsonProperty;
 public class WorkflowInvocation {
 
 	private String historyId, id, state, workflowId, updateTime;
+	
+	private List<Step> steps;
 	
 	private UUID uuid;
 
@@ -66,5 +69,40 @@ public class WorkflowInvocation {
 	public void setUpdateTime(String updateTime) {
 		//would like to use a Date object internally but couldn't get the parsing right
 		this.updateTime = updateTime;	
+	}
+	
+	public List<Step> getWorkflowSteps() {
+		return steps;
+	}
+	
+	@JsonProperty("steps")
+	public void setWorkflowSteps(List<Step> steps) {
+		this.steps = steps;
+	}
+	
+	@JsonIgnoreProperties(ignoreUnknown = true)
+	public static class Step {
+		String state;
+		
+		UUID uuid;
+
+		public String getState() {
+			return state;
+		}
+
+		@JsonProperty("state")
+		public void setState(String state) {
+			this.state = state;
+		}
+		
+		public UUID getUUID() {
+			return uuid;
+		}
+		
+		@JsonProperty("workflow_step_uuid")
+		public void setUUID(String uuid) {
+			this.uuid = UUID.fromString(uuid);
+		}
+		
 	}
 }
